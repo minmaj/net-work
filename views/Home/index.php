6908a44f5d810f6dfe60d47d5dfa6e9bcd67de3b
youvann <measkevin@gmail.com>
2015-02-03 18:05:24 +0100

    prob

--- views/Home/index.php
+++
@@ -1,6 +0,0 @@
-<div class="container">
-  <div class="cover-container">
-    <h1>Net-Work</h1>
-    <p><a class="btn btn-lg btn-success" href="administration" role="button">Accèder à l'application</a></p>
-  </div>
-</div>
