efd9642ca89260da9fa3ef20b77df406eafb1409
David <engdavid13@gmail.com>
2015-02-03 22:09:04 +0100

    division des parties de la vue d'administration anciennement regroupées dans views/administration/index.php en différents fichiers (components)

---
+++ views/administration/components/equipment_list.php
@@ -0,0 +1,108 @@
+<!-- navbar liste des équipements -->
+<div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav in" id="side-menu">
+            <li class="sidebar-search">
+                <div class="input-group custom-search-form">
+                    <input type="text" class="form-control" placeholder="Search...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+                <!-- /input-group -->
+            </li>
+            <li>
+                <a href="#" onclick="showstuffcontent('Ordinateurs fixes')"><i class="fa fa-desktop fa-fw"></i> Ordinateurs fixes <span class="badge">8</span></a>
+            </li>
+            <li>
+                <a href="#" onclick="showstuffcontent('Ordinateurs portables')"><i class="fa fa-laptop fa-fw"></i> Ordinateurs portables <span class="badge">2</span></a>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
+                <a href="#" onclick="showstuffcontent('Imprimantes')"><i class="fa fa-print fa-fw"></i> Imprimantes <span class="badge">4</span></a>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
+                <a href="#" onclick="showstuffcontent('Téléphones')"><i class="fa fa-phone fa-fw"></i> Téléphones <span class="badge">16</span></a>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
+                <a href="#" onclick="showstuffcontent('Routeurs')"><i class="fa fa-cloud fa-fw"></i> Routeurs <span class="badge">44</span></a>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
+                <a href="#" onclick="showstuffcontent('Serveurs')"><i class="fa fa-server fa-fw"></i> Serveurs <span class="badge">0</span></a>
+                <!-- /.nav-second-level -->
+            </li>
+        </ul>
+        <hr />
+        <div class="panel panel-primary" style="margin: 8px;">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-github fa-2x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right" style="padding-top:5px">
+                        <div>
+                            A propos de nous!
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="https://github.com/kazuia">
+                <div class="panel-footer">
+                    <span class="pull-left">
+
+                        Kazuia
+
+                    </span>
+                    <span class="pull-right">
+                        <i class="fa fa-arrow-circle-right"></i>
+                    </span>
+                    <div class="clearfix"></div>
+            </a>
+            <a href="https://github.com/minmaj">
+
+                <span class="pull-left">
+
+                    Minmaj
+
+                </span>
+                <span class="pull-right">
+                    <i class="fa fa-arrow-circle-right"></i>
+                </span>
+                <div class="clearfix"></div>
+            </a>
+            <a href="https://github.com/youvann">        
+                <span class="pull-left">
+
+                    Youvann
+
+                </span>
+                <span class="pull-right">
+                    <i class="fa fa-arrow-circle-right"></i>
+                </span>
+                <div class="clearfix"></div>
+            </a>
+            <br/>
+
+            <a href="https://github.com/minmaj/net-work">
+                <span class="pull-left">
+
+                    Contribuez !
+
+                </span>
+                <span class="pull-right">
+                    <i class="fa fa-arrow-circle-right"></i>
+                </span>
+                <div class="clearfix"></div>
+            </a>
+        </div>
+        </a>
+    </div>
+</div>
+<!-- /.sidebar-collapse -->
+</div>
+
---
+++ views/administration/components/equipment_table.php
@@ -0,0 +1,71 @@
+<div id="stuffcontent">
+    <h1 class="page-header">Ordinateurs fixes</h1>
+    <div class="alert alert-warning" role="alert">Attention! 8 ordinateurs fixes sont actuellement en pannes!</div>
+
+    <div class="panel panel-default"> 
+        <table id="stuffTable" class="table tablesorter">
+            <thead>
+                <tr class="blue-background">
+                    <th>ID</th>
+                    <th>NOM</th>
+                    <th>FABRIQUANT</th>
+                    <th>ADRESSE IP</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="warning-row">
+                    <td>1</td>
+                    <td>YOLO</td>
+                    <td>DOGE CORP.</td>
+                    <td>127.0.0.1</td>
+                    <td>
+                        <button type="button" class="btn btn-default btn-xs"
+                                data-toggle="modal"
+                                data-target="#viewDetailsModal">
+                            <i class="fa fa-search-plus"></i> View details
+                        </button>
+                        <button type="button" class="btn btn-info btn-xs">
+                            <i class="fa fa-edit"></i> Edit
+                        </button>
+                        <button type="button" class="btn btn-danger btn-xs"
+                                data-toggle="modal"
+                                data-target="#suppressionModal">
+                            <i class="fa fa-times"></i> Delete
+                        </button>
+                    </td>
+                </tr>
+                <tr class="valid-row">
+                    <td>2</td>
+                    <td>SWAG</td>
+                    <td>DOGE CORP.</td>
+                    <td>127.0.0.1</td>
+                    <td>
+                        <button type="button" class="btn btn-default btn-xs"
+                                data-toggle="modal"
+                                data-target="#viewDetailsModal">
+                            <i class="fa fa-search-plus"></i> View details
+                        </button>
+                        <button type="button" class="btn btn-info btn-xs">
+                            <i class="fa fa-edit"></i> Edit
+                        </button>
+                        <button type="button" class="btn btn-danger btn-xs"
+                                data-toggle="modal"
+                                data-target="#suppressionModal">
+                            <i class="fa fa-times"></i> Delete
+                        </button>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Bouton d'ajout -->
+    <button id="addStuffButton" onclick="showaddform()" type="button" class="btn btn-success" style="margin-bottom: 10px;">
+        <i class="fa fa-plus"></i> Ajouter
+    </button>
+    <button id="backHomeButton" onclick="showhomecontent()" type="button" class="btn btn-primary" style="margin-bottom: 10px;">
+        <i class="fa fa-reply"></i> Retour
+    </button>
+
+    <hr />
+</div>
