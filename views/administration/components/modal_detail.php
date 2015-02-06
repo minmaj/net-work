6680c4e85b9bc694a961890c48d54f38b1a21b41
Kazuia <Kazuia@pc-max>
2015-02-06 16:37:21 +0100

    modal detail display

--- views/administration/components/modal_detail.php
+++ views/administration/components/modal_detail.php
@@ -6,9 +6,23 @@
                 <h4 class="modal-title" id="viewDetailsModalLabel">Vue détaillée d'un équipement</h4>
             </div>
             <div class="modal-body">
-                <table>
-                    <thead></thead>
-                </table>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <span id="detail_title">X</span>
+                    </div>
+                    <div class="panel-body">
+                        <!-- SEQUENCE A METTRE EN BOUCLE
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <p>PLS</p>
+                            </div>
+                            <div>
+                                <p>DOGE ME</p>
+                            </div>
+                        </div>
+                        <hr/> -->
+                    </div>
+                </div>
             </div>
         </div>
     </div>
